ae1298b8d879d4545e49edf0db65b666002b4202
StekAlaZarii <101180506+StekAlaZarii@users.noreply.github.com>
2022-03-22 10:45:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    for(int i=50;i<101;i++){
+      if(i%2==0){
+        
+      }
+      else{
+        System.out.println(i);
+      }
+    }
+    
+  }
+}
